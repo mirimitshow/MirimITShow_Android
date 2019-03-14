3187be0ddc1567e7d5ae23be8447d8990e8b1933
HongEunbeen <vvvv980@naver.com>
2019-03-14 23:17:40 +0900

    fragment fixed

--- app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/MainActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/MainActivity.java
@@ -3,8 +3,10 @@ package s2017s40.kr.hs.mirim.mirimitshow;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager = getSupportFragmentManager();
     // 4개의 메뉴에 들어갈 Fragment들
     private GroupFragment groupFragment = new GroupFragment();
+    private MyPaPerFragment mypaperFragment = new MyPaPerFragment();
+    private MyHomeFragment myhomeFragment = new MyHomeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +26,32 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         // 첫 화면 지정
-        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frame_layout, groupFragment).commitAllowingStateLoss();
 
-        BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-                = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
+        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
                 switch (item.getItemId()) {
-                    case R.id.navigation_group:
+                    case R.id.navigation_group: {
                         transaction.replace(R.id.frame_layout, groupFragment).commitAllowingStateLoss();
-                        return true;
-                    case R.id.navigation_my_paper:
-
-                        return true;
-                    case R.id.navigation_my_hone:
-
-                        return true;
+                        break;
+                    }
+                    case R.id.navigation_my_paper: {
+                        transaction.replace(R.id.frame_layout, mypaperFragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_my_home: {
+                        transaction.replace(R.id.frame_layout, myhomeFragment).commitAllowingStateLoss();
+                        break;
+                    }
                 }
-                return false;
-            }
-        };
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+                return true;
+            }
+        });
+
+    }
 }
