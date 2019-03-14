3425a597d7a2414d8957a3251c86cfd84bdc1a9c
HongEunbeen <vvvv980@naver.com>
2019-03-14 23:52:37 +0900

    recyclerView test ok

--- app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/GroupAdapter.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/GroupAdapter.java
@@ -1,59 +1,54 @@
 package s2017s40.kr.hs.mirim.mirimitshow;
-//
-//import android.support.v7.widget.RecyclerView;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.ViewHolder> {
-//    private ArrayList<> mDataset;
-//
-//    // Provide a reference to the views for each data item
-//    // Complex data items may need more than one view per item, and
-//    // you provide access to all the views for a data item in a view holder
-//    public static class ViewHolder extends RecyclerView.ViewHolder {
-//        // each data item is just a string in this case
-//        public ImageView mImageView;
-//        public TextView mTextView;
-//
-//        public ViewHolder(View view) {
-//            super(view);
-//            mImageView = (ImageView)view.findViewById(R.id.image);
-//            mTextView = (TextView)view.findViewById(R.id.textview);
-//        }
-//    }
-//
-//    // Provide a suitable constructor (depends on the kind of dataset)
-//    public MyAdapter(ArrayList<MyData> myDataset) {
-//        mDataset = myDataset;
-//    }
-//
-//    // Create new views (invoked by the layout manager)
-//    @Override
-//    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-//                                                   int viewType) {
-//        // create a new view
-//        View v = LayoutInflater.from(parent.getContext())
-//                .inflate(R.layout.my_view, parent, false);
-//        // set the view's size, margins, paddings and layout parameters
-//        ViewHolder vh = new ViewHolder(v);
-//        return vh;
-//    }
-//
-//    // Replace the contents of a view (invoked by the layout manager)
-//    @Override
-//    public void onBindViewHolder(ViewHolder holder, int position) {
-//        // - get element from your dataset at this position
-//        // - replace the contents of the view with that element
-//        holder.mTextView.setText(mDataset.get(position).text);
-//        holder.mImageView.setImageResource(mDataset.get(position).img);
-//    }
-//
-//    // Return the size of your dataset (invoked by the layout manager)
-//    @Override
-//    public int getItemCount() {
-//        return mDataset.size();
-//    }
-//}
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.ViewHolder> {
+    private ArrayList<GroupDTO> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView_name;
+        public TextView mTextView_participants;
+
+        public ViewHolder(View view) {
+            super(view);
+            mImageView = (ImageView)view.findViewById(R.id.item_group_image);
+            mTextView_name = (TextView)view.findViewById(R.id.item_group_name_text);
+            mTextView_participants = (TextView)view.findViewById(R.id.item_group_participants_text);
+        }
+    }
+    public GroupAdapter(ArrayList<GroupDTO> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public GroupAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_re_view_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        int url =   Integer.parseInt(mDataset.get(position).getUrl());
+        holder.mImageView.setImageResource(url);
+        //mDataset.get(position).getName()
+        holder.mTextView_name.setText("dmsqls");
+        holder.mTextView_participants.setText("dmsqls");
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
