05ae6766ee29e64c2d5159a26ecae2186740623c
HongEunBeen <vvvv980@gmail.com>
2019-03-26 20:07:37 +0900

    학교 끝

--- app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/Group1Fragement.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/Group1Fragement.java
@@ -1,60 +1,45 @@
 package s2017s40.kr.hs.mirim.mirimitshow;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.net.URL;
 import java.util.ArrayList;
 
-
-public class GroupFragment extends Fragment {
-    public static GroupFragment newInstance() {
-            return new GroupFragment();
+public class Group1Fragement extends Fragment {
+    public Group1Fragement() {
+        // Required empty public constructor
     }
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<GroupDTO> myDataset;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_group, container, false);
+        View view = inflater.inflate(R.layout.fragment_group_1, container, false);
 
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.group_recycler_view);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.sub_group1_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         myDataset = new ArrayList<>();
         //어탭터
-        mAdapter = new GroupAdapter(myDataset, new GroupAdapter.ClickCallback() {
+        mAdapter = new GroupSub1Adapter(myDataset, new GroupSub1Adapter.ClickCallback() {
             @Override
             public void onItemClick(int position) {
-              //클릭 이벤트
-                Fragment fg;
-                fg = GroupSubFragment.newInstance();
-                setChildFragment(fg);
+                //클릭 이벤트
             }
         });
         mRecyclerView.setAdapter(mAdapter);
 
         myDataset.add(new GroupDTO("d",String.valueOf(R.mipmap.ic_launcher)));
-        return view;
-    }
-    private void setChildFragment(Fragment child) {
-        FragmentTransaction childFt = getChildFragmentManager().beginTransaction();
 
-        if (!child.isAdded()) {
-            childFt.replace(R.id.child_fragment_container, child);
-            childFt.addToBackStack(null);
-            childFt.commit();
-        }
+        return view;
     }
 }
