fc646c4f02d4b8b8db42502ef3b9b28b4f761d52
송승연 <wnzk413@gmail.com>
2019-05-20 10:33:56 +0900

    fab3 연결(scan class)

--- app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/MainActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/MainActivity.java
@@ -69,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 anim();
+                Intent intent = new Intent(MainActivity.this, ScanClassActivity.class);
+                startActivity(intent);
+
             }
         });
 
