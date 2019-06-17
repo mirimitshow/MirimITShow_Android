c3501df32c77199a82ca7bdc767287f389ad5824
HongEunBeen <vvvv980@gmail.com>
2019-06-17 12:09:32 +0900

    ip 주소 변경

--- app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/Utils.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/mirimitshow/Utils.java
@@ -41,7 +41,7 @@ public class Utils {
                 .build();
 
         mRetrofit  = new Retrofit.Builder()
-                .baseUrl("http://54.180.32.86:9000")
+                .baseUrl("http://13.209.211.81:9000")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
